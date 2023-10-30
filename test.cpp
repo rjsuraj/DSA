5a785bd5c3a37254cc2c7f8c0baaf3e556dc8181
Suraj Mandal <rjsurajmandal@gmail.com>
2023-10-30 23:11:37 +0530

    updated

--- test.cpp
+++ test.cpp
@@ -4,21 +4,5 @@ using namespace std;
 
 
 int main() {
-    int arr[] = {-2,-2,1,-2,0,-3,-4,-4,0};
-    int n = 9;
-    int max = *max_element(arr,arr+n);
-    sort(arr,arr+n);
-    int i;
-    for(i=1;i<=max;i++){
-            
-        // if(!(find(arr,arr+n,i) != arr+n))  //linear search
-        //     return i;
-
-        if( !(binary_search(arr,arr+n,i)) ){
-            cout<<"ans"<<i;
-            return 0;
-        } 
-    }
-        
-    cout<<i;
+    cout<< -1/2;
 }
