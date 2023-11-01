2fa6e37c4b1c11bfafb266d43462d41af3d49d64
Suraj Mandal <89829060+rjsuraj@users.noreply.github.com>
2023-11-01 10:44:18 +0530

    Time: 3 ms (58.38%), Space: 10.6 MB (24.52%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int findMin(vector<int>& arr) {
-        int n = arr.size();
-        //edge cases
-        if(n == 1)
-            return arr[0];
-        //if first element is minimum
-        if(arr[0]<arr[1] && arr[0]<arr[n-1])
-            return arr[0];
-        //if last element is minimum
-        if(arr[n-1]<arr[n-2] && arr[n-1]<arr[0])
-            return arr[n-1];
 
-        int start = 1,end = n-2, mid;
+        int start = 0 ,end = arr.size()-1,mid;
+        int ans = arr[0]; // when array is not rotated then first   element is minimum
+
         while(start <= end){
-            
-            mid = start + (end-start)/2;
-            
-            if(arr[mid]<arr[mid+1] && arr[mid]<arr[mid-1])
-                return arr[mid]; //minimum element
-            else if(arr[mid] > arr[0])
-                start = mid+1; //right part search
-            else 
-                end = mid-1;//left part search
 
+            mid = end + (start - end)/2;
+
+            //we are in left sorted part
+            if(arr[mid] >= arr[0]){
+                start = mid+1;             //right search
+            }
+            //we are in right sorted part
+            else{
+                ans = arr[mid];
+                end = mid-1;              //left search
+            }
+                
         }
-        return -1;
+        
+        return ans;
     }
 };
