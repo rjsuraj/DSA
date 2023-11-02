1f21a231be2b810ea7614b86e910b9ea2a13637d
Suraj Mandal <89829060+rjsuraj@users.noreply.github.com>
2023-11-02 17:21:20 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.53%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int specialArray(vector<int>& arr) {
-        int freq[1001] = {0};
+        //answer range lies between 1-arr.size()
+        int freq[101] = {0};
+        //count --> store count of the number >= to ans
         int count = 0 , ans = -1;
 
         for(int i=0; i<arr.size(); i++){
 
-            freq[arr[i]]++;
+            //we don't need to store the frequency of element which is  greater than arr size because ans lie in [1,arr.size()]
+            if(arr[i] <= arr.size())
+               freq[arr[i]]++;
 
             if(ans <= arr[i]){
 
